e02a518725231ed13f52f477602a2983b71438d0
Kyle Frost <kylefrost@comcast.net>
2015-03-18 01:25:20 +0000

    Add dev .py for local testing

---
+++ yakyik_dev.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request, jsonify
+from yikyak import YikYakAPI
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('index.html', title='YakYik')
+
+@app.route("/get_yaks", methods=['GET', 'POST'])
+def get_yaks():
+    lat = request.args.get('lat')
+    lng = request.args.get('lng')
+    
+    yyapi = YikYakAPI(None, lat, lng)
+    yyapi.registerUser()
+    yaks = yyapi.getMessages(lat, lng)
+    return jsonify(yaks)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
+
